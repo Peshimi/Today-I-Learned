d4db95bd2f54db2b84ac941e94f9824cca27bf2a
Nadia Lazarek <woj.dag@o2.pl>
2022-06-28 15:12:22 +0200

    call php function from javascript

---
+++ mysql-php/headers.php
@@ -0,0 +1,23 @@
+<?php
+function add($x, $y){
+  return $x + $y;
+}
+
+function minus($a, $b) {
+  return $a - $b;
+}
+?>
+
+<body>
+  <h1 id="header"></h1>
+  <script>
+    const result = <?php echo add(5, 3)?> ;
+    document.getElementById("header").innerHTML = result;
+  </script> 
+
+  <h1 id="header2"></h1>
+  <script>
+    const result2 = <?php echo minus(22, 9)?> ;
+    document.getElementById("header2").innerHTML = result2;
+  </script>
+</body>
