ac4e8172198101cf01026a633782e5a77ae75f3e
Nadia Lazarek <woj.dag@o2.pl>
2022-07-04 16:07:59 +0200

    added new property to discounta value and removed comments

--- php_studies/object_oriented/defining-methods.php
+++ php_studies/object_oriented/defining-methods.php
@@ -1,24 +1,22 @@
 <?php
 
-// 1. Create a Basket Class
 class Basket
 {
-  // 2. Add itemsTotal and shippingCost public Properties
   public $itemsTotal;
   public $shippingCost;
+  public $discount;
 
   public function calculateSubTotal() {
-    $subTotal = $this->itemsTotal + $this->shippingCost;
+    $subTotal = $this->itemsTotal + $this->shippingCost - $this->discount;
     return $subTotal;
   }
 }
 
-// 3. Instantiate a Basket using the new keyword
 $basket = new Basket();
-// 4. Set values for both properties
+
 $basket->itemsTotal = 50;
 $basket->shippingCost = 10;
-// 5. Use var_dump() to check it
+$basket->discount = 30;
 //var_dump($basket);
 
 //var_dump($basket->calculateSubTotal());
